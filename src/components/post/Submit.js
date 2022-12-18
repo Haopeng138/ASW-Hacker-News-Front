4a68e010d1a741bc2af0e73997e694bde1167610
Miguel <miguel.moreno.alcaraz@estudiantat.upc.edu>
2022-12-18 23:00:44 +0100

    Post empezado

--- src/components/post/Submit.js
+++ src/components/post/Submit.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
 export default class Submit extends Component {
     render(){
         return(
-            <p>prueba</p>
+            <>
+                <Card >
+                    <Card.Body>
+                        <Card.Text>
+                            title
+                        </Card.Text>
+
+                        <Card.Text>
+                            url
+                        </Card.Text>
+
+                        <Card.Text>
+                            text
+                        </Card.Text>
+
+                        <Card.Text>
+                            Leave url blank to submit a question for discussion. If there is no url, text will appear at the top of the thread. If there is a url, text is optional.
+                        </Card.Text>
+                        <Card.Text>
+                            You can also submit via <Card.Link href="#">bookmarklet</Card.Link>
+                        </Card.Text>
+                        <br></br>
+                    </Card.Body>
+                </Card>
+            </>
         );
     }
 }
