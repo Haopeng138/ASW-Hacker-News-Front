73c864b5fcf2f6438ce478b0fb8701a4dbe353d9
haopeng.lin <haopeng.lin@est.fib.upc.edu>
2022-12-23 00:42:42 +0100

    Updates

---
+++ precache-manifest.91871b1143d396aa96c679899596475d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7cb8c325e9cc95db690",
+    "url": "/ASW-Hacker-News-Front/static/css/main.fa1186fd.chunk.css"
+  },
+  {
+    "revision": "e7cb8c325e9cc95db690",
+    "url": "/ASW-Hacker-News-Front/static/js/main.e7cb8c32.chunk.js"
+  },
+  {
+    "revision": "13100166d48d32ba07bb",
+    "url": "/ASW-Hacker-News-Front/static/css/1.522f3b4d.chunk.css"
+  },
+  {
+    "revision": "13100166d48d32ba07bb",
+    "url": "/ASW-Hacker-News-Front/static/js/1.13100166.chunk.js"
+  },
+  {
+    "revision": "24b0527528538b23fed0",
+    "url": "/ASW-Hacker-News-Front/static/js/runtime~main.24b05275.js"
+  },
+  {
+    "revision": "20258564fc54be68ca602b87e3182ef1",
+    "url": "/ASW-Hacker-News-Front/index.html"
+  }
+];
