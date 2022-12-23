bd6a85a2198a02aeeae2a0d08821b2749f41a4db
Miguel <miguel.moreno.alcaraz@estudiantat.upc.edu>
2022-12-23 12:14:28 +0100

    prueba para deploy 2

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -16,8 +16,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3030;
 
-// your code
-
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`);
 });
